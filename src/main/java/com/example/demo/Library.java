830cd6fcb3e3bbd7aee46fe51b36ad7a5d19a912
taherwipro <66109361+taherwipro@users.noreply.github.com>
2020-06-22 21:48:33 +0530

    Create Library.java

---
+++ src/main/java/com/example/demo/Library.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public interface Library {
+  public static final String APP_NAME =  "Demo";
+  public static final String APP_VERSION = "1.0";
+  public static final long APP_ID = 100;
+}
